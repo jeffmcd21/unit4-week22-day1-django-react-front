2c7dd9262949be341f90730258c861a412287e5d
Jeff McDonald <jwmcdonald85@gmail.com>
2024-03-26 20:19:49 -0400

    wip: index and show layout

---
+++ src/pages/Index.jsx
@@ -0,0 +1,13 @@
+
+// Import our Pet Component
+
+function Index(props) {
+
+    return (
+        <>
+            <h1>Index</h1>
+        </>
+    )
+}
+
+export default Index
---
+++ src/pages/Show.jsx
@@ -0,0 +1,11 @@
+
+function Show(props) {
+
+    return (
+        <>
+            <h1>Show</h1>
+        </>
+    )
+}
+
+export default Show 
